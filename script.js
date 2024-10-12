3d4a6926f40e0e70af2952f9206bd439d30f25b7
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-10-12 20:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	getMakeModel(){
+		return make,model
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	getTopSpeed(){
+		return topSpeed
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
